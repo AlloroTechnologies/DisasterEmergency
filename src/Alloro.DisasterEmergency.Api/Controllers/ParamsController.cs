3f2084f7ca9854f33698943316dff7aea483a874
loreefran <alexis.henriquezb@gmail.com>
2023-10-07 20:27:51 -0300

    Added params controller

---
+++ src/Alloro.DisasterEmergency.Api/Controllers/ParamsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Alloro.DisasterEmergency.Api.Repositories;
+using Alloro.DisasterEmergency.Api.Models;
+
+namespace Alloro.DisasterEmergency.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParamsController : ControllerBase
+    {
+        private readonly DisasterEmergencyContext _dbContext;
+
+        public ParamsController(DisasterEmergencyContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetParams()
+        {
+            return Ok(new Params()
+            {
+                DisasterLevels = _dbContext.DisasterLevel.ToList(),
+                DisasterTypes = _dbContext.DisasterType.ToList(),
+                Resources = _dbContext.Resource.ToList()
+            });
+        }
+    }
+}
