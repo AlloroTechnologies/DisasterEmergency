0c2b2c5c8322741ad4b2ed8552ccc24f9708fa44
Alexis Henríquez <alexis.henriquezb@gmail.com>
2023-10-07 01:00:09 -0300

    Updated documentation and added application skeleton

---
+++ src/mobile/Alloro.DisasterEmergency.Mobile/Alloro.DisasterEmergency.Mobile/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿namespace Alloro.DisasterEmergency.Mobile;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.UseMauiMaps()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<WelcomeViewModel>();
+
+		builder.Services.AddSingleton<WelcomePage>();
+
+		builder.Services.AddSingleton<DisasterMapViewModel>();
+
+		builder.Services.AddSingleton<DisasterMapPage>();
+
+		builder.Services.AddSingleton<ReportDisasterViewModel>();
+
+		builder.Services.AddSingleton<ReportDisasterPage>();
+
+		// TODO: Add App Center secrets
+		AppCenter.Start(
+			"windowsdesktop={Your Windows App secret here};" +
+			"android={Your Android App secret here};" +
+			"ios={Your iOS App secret here};" +
+			"macos={Your macOS App secret here};",
+			typeof(Analytics), typeof(Crashes));
+
+		return builder.Build();
+	}
+}
